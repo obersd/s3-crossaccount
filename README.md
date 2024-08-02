80f4505d0e998b08f952dda2fd06bb16deab9717
lex.vargas <obersd@gmail.com>
2024-08-01 21:59:47 -0600

    chore: Update S3 bucket name in code to match new bucket name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # s3-crossaccount
-This repo contains an exercise that demonstrates how to create and configurate an S3 Bucket with Serverless Framework, and then, upload object from another AWS Account.
+This repo contains an exercise that demonstrates how to create and configure an S3 Bucket with Serverless Framework, and then, upload object from another AWS Account.
--- account-2/ConsoleApp1/Program.cs
+++ account-2/ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ using Amazon.S3.Transfer;
 
 var credentials = new BasicAWSCredentials("ak", "sk");
 var s3Client = new Amazon.S3.AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
-var bucketName = "mi-bucket-compartido";
+var bucketName = "my-public-bucket-store";
 var ftu = new TransferUtility(s3Client);
 await ftu.UploadAsync("test.png", bucketName);
 Console.WriteLine("OK");

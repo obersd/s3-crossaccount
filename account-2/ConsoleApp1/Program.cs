2b320486ad3d0126a7ffd154c29055b40527827f
lex.vargas <obersd@gmail.com>
2024-08-01 21:57:59 -0600

    chore: Add AWS S3 bucket configuration for account-1 and account-2

---
+++ account-2/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Amazon.Runtime;
+using Amazon.S3.Transfer;
+
+var credentials = new BasicAWSCredentials("ak", "sk");
+var s3Client = new Amazon.S3.AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
+var bucketName = "mi-bucket-compartido";
+var ftu = new TransferUtility(s3Client);
+await ftu.UploadAsync("test.png", bucketName);
+Console.WriteLine("OK");
